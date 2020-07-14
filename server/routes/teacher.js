97b89b12e8535a0e209bc2c14dabce53d75f3fc6
Houssem Eddine <46385752+360gunner@users.noreply.github.com>
2020-07-14 18:25:43 +0200

    trying aws s3

--- server/routes/teacher.js
+++ server/routes/teacher.js
@@ -6,6 +6,8 @@ const multer = require('multer');
 const multerS3 = require('multer-s3');
 const s3 = new aws.S3({ accessKeyId: "AKIAIBR5G5OP47EVSYJA", secretAccessKey: "mXU0TGX4NV0QXUsD2J8iwtJi9sSQmHSeEU9j2bqe" });
 
+
+
 const faker = require('faker');
 
 const checkJWT = require('../middlewares/check-jwt');
