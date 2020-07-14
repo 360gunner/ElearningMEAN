1ef4f6a471cdc8bb8d2016743b2805c398124f41
Houssem Eddine <46385752+360gunner@users.noreply.github.com>
2020-07-14 22:02:17 +0200

    Delete config.js

--- server/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  database:
-   // 'mongodb://arash:abc123@ds111638.mlab.com:11638/Biapiwebapplication',
-   //'mongodb+srv://360gunner:1998@cluster0.pxcve.mongodb.net/biapi?retryWrites=true&w=majority',
-   //'mongodb://360gunner:1998@cluster0.pxcve.mongodb.net/biapi?retryWrites=true&w=majority',
-   'mongodb://localhost:27017/admin',
-   port: 3030,
-  
-};
